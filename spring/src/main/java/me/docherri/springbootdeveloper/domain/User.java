c03f9086bc1918730ea9b646602c7dc98322f51a
doyunnii <dyeono@hansung.ac.kr>
2024-10-22 10:12:46 +0900

    Add files via upload

---
+++ spring/src/main/java/me/docherri/springbootdeveloper/domain/User.java
@@ -0,0 +1,44 @@
+package me.docherri.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+
+    public enum Gender {
+        MALE, FEMALE
+    }
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "uid", updatable = false, unique = true)
+    private long uid;
+
+    @Column(name = "loginId", nullable = false)
+    private String loginId;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender", nullable = false)
+    private Gender gender;
+
+    @Column(name = "profileMessage", nullable = true)
+    private String profileMessage;
+
+    @Column(name = "profilePic", nullable = true)
+    private String profilePic;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "todolist_id", nullable = true)
+    private TodoList todoList;
+}
