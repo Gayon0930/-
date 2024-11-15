f58d788523927fd78844ec08f9bac16519646d9f
Gayon0930 <159022587+Gayon0930@users.noreply.github.com>
2024-11-15 14:42:45 +0900

    Delete main directory

--- main/java/com/example/lobby/controller/RoomRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.lobby.controller;
-
-import lombok.Data;
-
-@Data // Lombok 어노테이션으로 getter, setter, toString, equals 등을 자동 생성
-public class RoomRequest {
-    private String name;
-}
