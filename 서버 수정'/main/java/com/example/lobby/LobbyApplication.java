789e7a5ae71ba04bbfe089eadd653a5d3b021e01
Gayon0930 <159022587+Gayon0930@users.noreply.github.com>
2024-11-15 14:42:02 +0900

    Add files via upload

---
+++ 서버 수정'/main/java/com/example/lobby/LobbyApplication.java
@@ -0,0 +1,11 @@
+package com.example.lobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LobbyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LobbyApplication.class, args);
+    }
+}
